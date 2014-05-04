5ed173ce6283652170cb7fa65736db9a2a232f43
Dan <git@dandart.co.uk>
2014-05-04 20:35:32 +0000

    Some help

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
 (function() {
     var App = function() {
+        /*
+         * ?Action=ScriptsList
+         * &Version=2.0.0
+         * &KeyID=<Your Scalr API Key ID>
+         * &TimeStamp=2009-06-19T05%3A13%3A00.000Z
+         * &Signature=<URLEncode(Base64Encode(Signature))>
+         *
+         * ?Action=ScriptExecute
+         * &FarmID=126
+         * &ScriptID=100
+         * &Timeout=30
+         * &Revision=3
+         * &ConfigVariables[var1]=/home/www
+         * &ConfigVariables[var2]=test
+         * &Async=1
+         * &Version=2.0.0
+         * &AuthVersion=2
+         * &KeyID=<Your Scalr API Key ID>
+         * &TimeStamp=2009-06-19T05:13:00.000Z
+         * &Signature=<URLEncode(Base64Encode(Signature))>
+
         this.start = function() {
             if (!this.isLoggedIn()) {
                 this.render('login');
             }
         }.bind(this);
+
+        this.genSignature = function(action, keyid, timestamp) {
+            
+        };
         
         this.isLoggedIn = function() {
             return false;
